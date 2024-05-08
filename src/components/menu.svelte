8adc1b03980d42365ceb12ceb747c608b794f827
vitalspace <myglobalsounds@gmail.com>
2024-05-08 00:13:36 -0600

    updated

--- src/components/menu.svelte
+++ src/components/menu.svelte
@@ -2,14 +2,13 @@
   console.log("Hello world");
 </script>
 
-<header class="fixed w-full z-10">
-  <nav class="flex justify-between container mx-auto py-10 text-white">
+<header class="fixed px-20 justify-center z-10 w-full ">
+  <nav class="flex justify-between py-10 text-white">
     <a href="/" class="flex">
       <img src="/zig-logo-light.svg" width="50" alt="" />
       <h2 class="text-xl font-mono">.lat</h2>
     </a>
     <ul class=" flex space-x-10">
-      <li><a href="/">Home</a></li>
       <li><a href="/documentacion">Documentacion</a></li>
       <li><a href="/aprende">Aprende</a></li>
       <li><a href="/colaboradores">Colaboradores</a></li>
@@ -22,7 +21,7 @@
         >
       </li>
       <li>
-        <a href="/discor" class="border-2 p-2 border-gray-400">Discord</a>
+        <a href="/discord" class="border-2 p-2 border-gray-400">Discord</a>
       </li>
     </ul>
   </nav>
