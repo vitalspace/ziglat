de11a8f4490b886e91830b927ecaa12784006ca9
vitalspace <myglobalsounds@gmail.com>
2024-05-09 13:21:40 -0600

    added over view section

---
+++ src/components/buttons/next_button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let text: string;
+  export let url: string;
+  export let hidden: boolean;
+</script>
+
+{#if hidden}
+  <a class="hidden" href={url}>
+    {text}
+  </a>
+{:else}
+  <a class="bg-purple-500 rounded-md border block text-center py-2 hover:bg-purple-800 transition-all" href={url}>
+    {text}
+  </a>
+{/if}
